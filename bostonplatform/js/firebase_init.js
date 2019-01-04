986f58d1fca8377721f457debea49fa13122ea5c
Miffy Chen <mchen15@bu.edu>
2019-01-04 08:55:13 -0500

    temp host for project: bostonplatform

---
+++ bostonplatform/js/firebase_init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAAOqiqMrw3utPi2e7HemDN-YXI8tOajG0",
+    authDomain: "bostonplatform2018.firebaseapp.com",
+    databaseURL: "https://bostonplatform2018.firebaseio.com",
+    projectId: "bostonplatform2018",
+    storageBucket: "bostonplatform2018.appspot.com",
+    messagingSenderId: "538071414774"
+};
+firebase.initializeApp(config);
