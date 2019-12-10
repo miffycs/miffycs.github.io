d83dba0b4f4af6e3e4abb21f8afd94fd16b7620a
Miffy Chen <mchen15@bu.edu>
2019-12-10 05:57:05 -0500

    Remove temp folder

--- final/js/loadPageAttributes.js
+++
@@ -1,6 +0,0 @@
-const url_page_attr = "https://gist.githubusercontent.com/miffycs/3918490c03cd9a1e96926973f636b102/raw/b3abac9ce770f472872b34211057a351738a5af0/page_attributes.html";
-
-window.onload = function() {
-    $("nav").load(`${url_page_attr} nav`);
-    $("footer").load(`${url_page_attr} #footer-details`);
-};
