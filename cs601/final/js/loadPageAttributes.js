f17351e70a0d35ac8258f540c97b63df05c27f81
Miffy Chen <mchen15@bu.edu>
2019-12-09 21:44:25 -0500

    delete everything and refresh server

--- cs601/final/js/loadPageAttributes.js
+++
@@ -1,6 +0,0 @@
-const url_page_attr = "https://miffychen.tech/cs601/final/page_attributes.html";
-
-window.onload = function() {
-    $("nav").load(`${url_page_attr} nav`);
-    $("footer").load(`${url_page_attr} #footer-details`);
-};
